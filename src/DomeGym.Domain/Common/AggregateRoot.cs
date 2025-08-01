59134bb8ce881c355de53ed8b7d2732249e1f569
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2025-08-01 13:06:23 +0300

    Add EventualConsistencyMiddleware

--- src/DomeGym.Domain/Common/AggregateRoot.cs
+++ src/DomeGym.Domain/Common/AggregateRoot.cs
@@ -6,5 +6,13 @@ public abstract class AggregateRoot : Entity
     {
     }
 
+    protected readonly List<IDomainEvent> _domainEvents = new();  
+    public List<IDomainEvent> PopDomainEvents()
+    {
+        var copy = _domainEvents.ToList();
+        _domainEvents.Clear();
+
+        return copy;
+    }
     protected AggregateRoot() { }
 }
---
+++ src/DomeGym.Infrastructure/Middleware/EventualConsistencyMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using DomeGym.Domain.Common;
+using DomeGym.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+using DomeGym.Domain.Common;
+namespace DomeGym.Infrastructure.Middleware
+{
+    public class EventualConsistencyMiddleware
+    {
+        public const string DomainEventsKey = "DomainEventsKey";
+
+        private readonly RequestDelegate _next;
+
+        public EventualConsistencyMiddleware(RequestDelegate next)
+        {
+            _next = _next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public async Task InvokeAsync(
+            HttpContext context,
+            IPublisher publisher,
+            DomeGymDbContext dbContext)
+        {
+            var transaction = await dbContext.Database.BeginTransactionAsync();
+            context.Response.OnCompleted(async () =>
+            {
+                try
+                {
+                    if (context.Items.TryGetValue(DomainEventsKey, out var value) 
+                        && value is Queue<IDomainEvent> domainEvents)
+                    {
+                        while (domainEvents.TryDequeue(out var nextEvent))
+                        {
+                            await publisher.Publish(nextEvent);
+                        }
+                    }
+                    await transaction.CommitAsync();
+
+
+                }
+                catch(EventualConsistencyException e)
+                {
+                    
+                }
+                finally
+                {
+                    await transaction.DisposeAsync();
+                }
+
+            });
+              await _next(context);
+        }
+    }
+}
